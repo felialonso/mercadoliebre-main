5c90f3478d23f594f21612aa7d1e542512bc1938
felialonso <felipealonsosznec@gmail.com>
2022-12-19 21:27:28 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+
+
+app.listen(3010, ()=>{
+    console.log('Servidor corriendo en el puerto 3010');
+})
