a351e3e362600ef8c4bfc5418a75f2d70a2724a5
felialonso <felipealonsosznec@gmail.com>
2023-01-03 02:25:39 -0300

    agrego esto

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
+app.get('/register', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+
+app.get('/home', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
 
 
 app.listen(3010, ()=>{
