6dd8c15d9b703634a5273bdfbacd2cfbb9e932a9
felialonso <felipealonsosznec@gmail.com>
2023-01-04 16:18:30 -0300

    se agrega esto

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ app.get('/register', (req, res)=> {
 })
 
 
-app.get('/home', (req, res)=> {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+app.get('/login', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
 })
+
+
 
 
 
